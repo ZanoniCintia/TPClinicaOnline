21cff3c283cbd052dadabe4f1123b2eb7c33f0ee
cintu94@hotmail.com <cintu94@hotmail.com>
2025-04-13 18:57:53 -0300

    ultimo commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,28 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './componentes/login/login.component';
+import { LoginComponent} from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { PageNotFoundComponent } from './componentes/page-not-found/page-not-found.component';
+
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
 
 
 export const routes: Routes = [
+    // Si le ponemos 'prefix' nos va a arrojar un error en la consola de redireccion infinita
+    { path: '', redirectTo: '/home', pathMatch: "full" },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
     {
-        path: 'login',
-        component: LoginComponent,
+        path: 'quien-soy', component: QuienSoyComponent ,
+       /* children:
+            [
+                {
+                    path: "detalle/:productId",
+                    component: ProductDetailComponent
+                }
+            ]*/
     },
-    {
-        path: 'home',
-        loadComponent: () => import('./componentes/home/home.component').then(m => m.HomeComponent),
-    },
-    {
-        path: 'quien-soy',
-        loadComponent: () => import('./componentes/quien-soy/quien-soy.component').then(m => m.QuienSoyComponent),
-    },
-  
+    // La ruta comodin debe ir siempre al final
+    { path: '**', component: PageNotFoundComponent },
+    
 ];
 
