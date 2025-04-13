c95505292daa6010a8e367e25e6a5710908c246f
cintu94@hotmail.com <cintu94@hotmail.com>
2025-04-13 14:52:43 -0300

    Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./componentes/home/home.component').then(m => m.HomeComponent),
+    },
+    {
+        path: 'quien-soy',
+        loadComponent: () => import('./componentes/quien-soy/quien-soy.component').then(m => m.QuienSoyComponent),
+    },
+  
+];
+
