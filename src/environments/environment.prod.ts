6d879bbd297b64bcdef9e255e532480fc9a76a14
cintu94@hotmail.com <cintu94@hotmail.com>
2025-04-15 20:20:27 -0300

    hoy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
+
 export const environment = {
-    production: false,
+    production: true,
     apiUrl: 'https://okhubqbtmacszztmqiki.supabase.co',
     publicAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9raHVicWJ0bWFjc3p6dG1xaWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0OTU3MzUsImV4cCI6MjA2MDA3MTczNX0.wqdmRIa6Nq3-cS_p7dF1R2E_ZHsYZ0XPuB7_SqCBhs0'
+  
+   
+
+  
   };
+
+  
+  
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,10 @@
+
 export const environment = {
-    production: true,
+    production: false,
     apiUrl: 'https://okhubqbtmacszztmqiki.supabase.co',
     publicAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9raHVicWJ0bWFjc3p6dG1xaWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0OTU3MzUsImV4cCI6MjA2MDA3MTczNX0.wqdmRIa6Nq3-cS_p7dF1R2E_ZHsYZ0XPuB7_SqCBhs0'
-  };
   
+  
+  };
+
+ 
